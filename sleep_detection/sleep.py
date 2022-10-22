9b28fd7f8b21c09d26f20f4f51b823f60f69015a
KKcuber <kkcubes@gmail.com>
2022-10-23 01:29:29 +0530

    hi

---
+++ sleep_detection/sleep.py
@@ -0,0 +1,89 @@
+import winsound
+import tensorflow as tf
+frequency = 2500  # Set frequency to 2500
+duration = 1500  # Set duration to 1500 ms == 1.5 sec
+import numpy as np
+import cv2
+new_model = tf.keras.models.load_model('C:/Users/Kushagra/programming/Megathon-22/sleep_detection/my_model.h5')
+path = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+cap = cv2.VideoCapture(1)
+# eyes_roi = np.empty(2)
+#check if webcam is opened correctly
+if not cap.isOpened():
+    cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+cap.set(cv2.CAP_PROP_FPS, 5)
+counter = 0
+while True:
+    ret,frame = cap.read()
+    # frame = cv2.resize(frame, (224, 224))
+    cv2.imshow('Input', frame)
+    eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    eyes = eye_cascade.detectMultiScale(gray, 1.1, 4)
+    print(eyes)
+    if(len(eyes) == 0):
+        continue
+    for x,y,w,h in eyes:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0, 255, 0), 2)
+        eyess = eye_cascade.detectMultiScale(roi_gray)
+        if len(eyess) == 0:
+            print("Eyes are not detected")
+        else:
+            print("hi")
+            for (ex, ey, ew, eh) in eyess:
+                eyes_roi = roi_color[ey: ey+eh, ex: ex+ew]
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if(faceCascade.empty()==False):
+        print("detected")
+    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+    # Draw a rectangle around eyes
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    final_image = cv2.resize(eyes_roi, (224,224))
+    final_image = np.expand_dims(final_image, axis=0)
+    final_image = final_image/255.0
+    Predictions = new_model.predict(final_image)
+    if (Predictions>=0.3):
+        status = "Open Eyes"
+        cv2.putText(frame,
+                status,
+                (150,150),
+                font, 3,
+                (0, 255, 0),
+                2,
+                cv2.LINE_4)
+        x1,y1,w1,h1 = 0,0,175,75
+        cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1), (0,0,0), -1)
+        #Add text
+        cv2.putText(frame, 'Active', (x1 + int(w1/10),y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
+    elif Predictions<0.3:
+        counter = counter + 1
+        status = "Closed Eyes"
+        cv2.putText(frame,
+                status,
+                (150,150),
+                font, 3,
+                (0, 0, 255),
+                2,
+                cv2.LINE_4)
+        x1,y1,w1,h1 = 0,0,175,75
+        cv2.rectangle(frame, (x1,y1), (x1 + w1, y1 + h1), (0,0,255), 2)
+        if counter > 10:
+            x1,y1,w1,h1 = 0,0,175,75
+            #Draw black background rectangle
+            cv2.rectangle(frame, (x1, y1), (x1 + w1, y1 + h1), (0,0,0), -1)
+            #Add text
+            cv2.putText(frame, "Sleep Alert !!!", (x1 + int(w1/10), y1 + int(h1/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+            winsound.Beep(frequency, duration)
+            counter = 0
+    cv2.imshow("Drowsiness Detection", frame)
+    if cv2.waitKey(2) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
